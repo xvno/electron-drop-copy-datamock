5254986c9c4c9ae3daf8a34a6a1cf10c962d928b
weiboyang <weiboyang@movtile.com>
2019-10-24 20:10:52 +0800

    MISC: assets/smb.js

--- assets/smb.js
+++ assets/smb.js
@@ -5,11 +5,12 @@ var SMB2 = require('@marsaud/smb2');
 let username = decodeURI('alice');
 var smb2Client = new SMB2({
     share: '\\\\10.10.1.99\\alice',
-    domain: 'WORKGROUP',
-    username: 'alicesss',
-    password: 'Qwer1234',
-    autoCloseTimeout: 0
+    domain: '.',
+    username: 'alice',
+    password: 'Qwer_1234'
 });
+
+// autoCloseTimeout: 0
 /*
 smb2Client.readFile('readme.txt', function(err, content) {
     if (err) throw err;
@@ -19,11 +20,16 @@ smb2Client.readFile('readme.txt', function(err, content) {
 // With promise, ideal with ES2017 async functions
 //   const content = await smb2Client.readFile('foo.txt');
 //   console.log(content);
-
-smb2Client.exists('readme.txt', function(err, exists) {
+smb2Client.readdir('\\', function(err, files) {
     if (err) throw err;
-    console.log(exists ? "it's there" : "it's not there!");
-    smb2Client.close();
+    console.log(files);
+    smb2Client.disconnect();
 });
+
+// smb2Client.exists('readme.txt', function(err, exists) {
+//     if (err) throw err;
+//     console.log(exists ? "it's there" : "it's not there!");
+//     smb2Client.close();
+// });
 
 // export default smb2Client;
