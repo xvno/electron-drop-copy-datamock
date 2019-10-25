e849d0d69c6e9e3f658f6156e2f3b02f502c4c57
weiboyang <weiboyang@movtile.com>
2019-10-25 15:27:07 +0800

    Mdf: mac/cmd.js: 添加umount代码

--- mac/cmd.js
+++ mac/cmd.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const cp = require('child_process');
+const CP = require('child_process');
 const config = require('../config/smb.conf.js');
 const { host, share, username, password } = config;
 
@@ -26,24 +26,31 @@ function checkMountPoint() {
 
 // mount -t smbfs smb://alice:Qwer_1234@10.10.1.99/alice ~/._MOVTILE_SMB_001
 
-function checkMount() {
-    let buf = cp.execSync(`mount | grep "//${username}@${host}/${share}`);
+module.exports = function mountFS() {
+    let buf = CP.execSync(`mount | grep "//${username}@${host}/${share}`);
     let str = buf.toString();
     let reg = new RegExp(`${macMountPoint}$`);
     let ret = reg.test(str);
+    let umountCMD = `diskutil unmount ${macMountPoint}`;
+
     if (ret) {
-        return true;
+        return {
+            basePath: macMountPoint,
+            umount() {
+                CP.execSync(umountCMD);
+            }
+        };
     } else {
         if (checkMountPoint()) {
             mount({ host, share, username, password, macMountPoint });
         }
     }
-}
+};
 
 function mount(opt) {
     const { host, share, username, password, macMountPoint } = opt;
     let mountCMD = `mount -t smbfs smb://${username}:${password}@${host}/${share} ${macMountPoint}`;
-    let ret = cp.execSync(mountCMD);
+    let ret = CP.execSync(mountCMD);
     let str = ret.toString();
     let reg = new RegExp(`${macMountPoint}$`);
     if (reg.test(str)) {
@@ -52,5 +59,3 @@ function mount(opt) {
         throw new Error('Mount failed!');
     }
 }
-
-module.exports = checkMount;
